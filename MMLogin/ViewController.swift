7422d9b726cb8fd95137b5fa36d00e6ae0c6f0b9
zhegu <lizhe707@163.com>
2020-05-06 09:47:58 +0800

    Update ViewController.swift
    111

--- MMLogin/ViewController.swift
+++ MMLogin/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         title = "测试"
+        
+        //冲突处理
     }
     
     override func didReceiveMemoryWarning() {
